24ce41b8078a42419878601d11b7be3ffc5629b1
Kirill Hmelnitski <hmelnitskiy.k@intechs.by>
2023-10-24 23:08:11 +0300

    add 2015-day4-part2

--- 2015/day-04/src/main.rs
+++ 2015/day-04/src/main.rs
@@ -1,18 +1,15 @@
 fn main() {
-    let x = part1("abcdef");
-    println!("{:?}", x);
+    println!("part1 result: {:?}", part1("bgvyzdsv"));
+    println!("part1 result: {:?}", part2("bgvyzdsv"));
 }
 
-fn part1(input: &str) -> md5::Digest {
-    let mut data = (0..1_000_000).map(|number| {
+fn get_lowest_number_for_hash(input: &str, start_with: &str) -> (i32, md5::Digest) {
+    let mut data = (0..10_000_000).map(|number| {
         let data = format!("{}{}", input, number);
         let hash = md5::compute(data);
-        // println!("{:?}", hash);
-        // let x = format!("{:?}", hash.iter().take(5).collect::<Vec<&u8>>());
-        println!("{}", number);
         let collect = format!("{:?}", hash);
-        match collect.starts_with("00000") {
-            true => Some(hash),
+        match collect.starts_with(start_with) {
+            true => Some((number, hash)),
             false => None,
         }
     });
@@ -23,14 +20,29 @@ fn part1(input: &str) -> md5::Digest {
     }
 }
 
+fn part1(input: &str) -> (i32, md5::Digest) {
+    get_lowest_number_for_hash(input, "00000")
+}
+
+fn part2(input: &str) -> (i32, md5::Digest) {
+    get_lowest_number_for_hash(input, "000000")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn correct_part1() {
-    //     let input = "abcdef";
-    //     let actual = part1(input);
-    //     assert_eq!(actual, "asd")
-    // }
+    #[test]
+    fn correct_part1_v1() {
+        let input = "abcdef";
+        let (actual, _) = part1(input);
+        assert_eq!(actual, 609043)
+    }
+
+    #[test]
+    fn correct_part1_v2() {
+        let input = "pqrstuv";
+        let (actual, _) = part1(input);
+        assert_eq!(actual, 1048970)
+    }
 }
