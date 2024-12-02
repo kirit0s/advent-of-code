bd3c92cc894979a48e035e437cb052b23741fecd
Kirill Hmelnitski <hmelnitskiy.k@intechs.by>
2024-12-02 22:53:37 +0300

    add 2024-day1-part2

--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -8,7 +9,7 @@ fn main() {
     let input = parse_input(BufReader::new(file).lines().map(Result::unwrap));
 
     println!("part1: {}", part1(&input.0, &input.1));
-    println!("part2: todo!()");
+    println!("part2: {}", part2(&input.0, &input.1));
 }
 
 fn parse_input(input: impl Iterator<Item = String>) -> (Vec<i32>, Vec<i32>) {
@@ -40,9 +41,30 @@ fn part1(list1: &[i32], list2: &[i32]) -> u32 {
     list1.iter().zip(list2).map(|x| x.0.abs_diff(x.1)).sum()
 }
 
+fn part2(list1: &[i32], list2: &[i32]) -> i32 {
+    let list2_by_number = list2
+        .iter()
+        .fold(HashMap::<i32, i32>::new(), |mut acc, &x| {
+            acc.entry(x).and_modify(|x| *x += 1).or_insert(1);
+            acc
+        });
+
+    list1
+        .iter()
+        .map(|&x| list2_by_number.get(&x).map(|&count| x * count).unwrap_or(0))
+        .sum()
+}
+
 #[test]
 fn correct_part1() {
     let file = File::open("src/input_test.txt").unwrap();
     let input = parse_input(BufReader::new(file).lines().map(Result::unwrap));
-    assert_eq!(part1(&input.0, &input.1), 11u32);
+    assert_eq!(part1(&input.0, &input.1), 11);
+}
+
+#[test]
+fn correct_part2() {
+    let file = File::open("src/input_test.txt").unwrap();
+    let input = parse_input(BufReader::new(file).lines().map(Result::unwrap));
+    assert_eq!(part2(&input.0, &input.1), 31);
 }
